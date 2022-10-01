54486a8a7971c4af1decf827b23d242cc81f6138
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-10-01 15:55:51 +0300

    modified controller file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :dog_houses, only: [:show]
   resources :reviews, only: [:show, :index, :create]
+  resources :reviews, only: [:show, :index]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
